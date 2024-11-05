3381c947013afa8cd8f4ad029d955121ac98a702
Niall Begley <niall.begley@sonos.com>
2024-11-05 15:08:01 -0500

    initial

---
+++ Sources/DeleteThis/DeleteThis.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import CocoaLumberjackSwift
+
+public class DeviceEqSetting {
+
+    public func test() {
+        DDLog.add(DDOSLogger.sharedInstance)
+    }
+}
